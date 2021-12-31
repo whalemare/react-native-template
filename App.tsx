cf19169c2c79d0c74d6e87e593b3c96e662242c4
Anton Vlasov <nanostet@gmail.com>
2021-12-31 15:31:55 +0700

    Add ESlint

--- App.tsx
+++ App.tsx
@@ -1,22 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React from 'react';
-import {Text} from 'react-native';
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import React from 'react'
+import { Text } from 'react-native'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 export const App = () => {
   return (
-    <GestureHandlerRootView
-      style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <GestureHandlerRootView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Hello from React-Native template</Text>
     </GestureHandlerRootView>
-  );
-};
+  )
+}
